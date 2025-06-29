cef28b70faddfbe89488704fcf485e5ae130a03b
Nikolai Maklakov <168437204+mcluckoff@users.noreply.github.com>
2025-06-29 13:28:57 +0200

    ros2_control
    urdf model -> gazebo simulation -> lidar simulation -> ros2 control simulation
    
    !next up is hardware interface implementation

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -22,7 +22,7 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
     )
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
@@ -39,11 +39,23 @@ def generate_launch_description():
                                    '-entity', 'my_bot'],    
                         output='screen')
 
+    ackermann_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["acker_cont"],
+    )
 
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
 
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        ackermann_drive_spawner,
+        joint_broad_spawner
     ])
